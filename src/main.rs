c3fc8781533ee6d63bab1b67ec9340b99f545806
Gabriel Korherr <gabrielkorherr@gmail.com>
2024-01-24 02:37:01 +0100

    First attempt at a bottom-up parser, no it doesn't work

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,21 @@
 mod linked_list;
 mod fibonacci;
 mod syntax_tree;
+mod parse;
 
 use linked_list::ListLinked;
 use fibonacci::*;
 use syntax_tree::SyntaxTree;
 
 fn main() {
-    let tree = SyntaxTree::with_children(
-        "term",
-        vec![
-            SyntaxTree::new("("),
-            SyntaxTree::new("5"),
-            SyntaxTree::new("/"),
-            SyntaxTree::new("0"),
-            SyntaxTree::new(")")]
-    );
-    println!("{tree}");
+    let input = vec![
+        String::from("banana"),
+        String::from("+"),
+        String::from("banana")
+    ];
+
+    match parse::parse_term(&input) {
+        None => println!("no banana"),
+        Some(result) => println!("{result}")
+    }
 }
--- src/parse.rs
+++ src/parse.rs
@@ -1,9 +1,70 @@
-pub fn parse_term(input: &str) -> Option<SyntaxTree> {
-    SyntaxTree::new("term")
+use std::iter::zip;
+
+use crate::SyntaxTree;
+
+pub fn parse_term(input: &Vec<String>) -> Option<SyntaxTree> {
+    let rules = [
+        GrammarRule {
+            name: String::from("Var"),
+            production: Production { components: vec![String::from("banana")] }
+        },
+        GrammarRule {
+            name: String::from("Term"),
+            production: Production { components: vec![String::from("Var")] }
+        },
+        GrammarRule {
+            name: String::from("Term"),
+            production: Production { components: vec![String::from("("), String::from("Term"), String::from(")")] }
+        },
+        GrammarRule {
+            name: String::from("Term"),
+            production: Production { components: vec![String::from("Term"), String::from("+"), String::from("Term")] }
+        }
+    ];
+    let mut syntaxed_input: Vec<SyntaxTree> = input.iter().map(|s| SyntaxTree::new(s)).collect();
+    let mut last_count = syntaxed_input.len();
+
+    while syntaxed_input.len() > 1 {
+        syntaxed_input = parse_partial(syntaxed_input, &rules);
+        if syntaxed_input.len() == last_count {
+            return None;
+        }
+        last_count = syntaxed_input.len();
+    }
+    Some(syntaxed_input.remove(0))
 }
 
-pub fn parse(input: &str, rule: GrammarRule) -> Option<SyntaxTree> {
-
+fn parse_partial(mut input: Vec<SyntaxTree>, rules: &[GrammarRule]) -> Vec<SyntaxTree> {
+    let mut result = Vec::new();
+    let iter = input.iter();
+    let iter_2 = iter.clone();
+    while input.len() > 0 {
+        let mut match_found = false;
+        for rule in rules {
+            match_found = true;
+            if rule.production.components.len() <= input.len() {
+                for i in 0..rule.production.components.len() {
+                    if rule.production.components[i] != input[i].name() {
+                        match_found = false;
+                    }
+                }
+            } else {
+                match_found = false;
+            }
+            if match_found {
+                let mut components = Vec::new();
+                for _ in 0..rule.production.components.len() {
+                    components.push(input.remove(0));
+                }
+                result.push(SyntaxTree::with_children(&rule.name, components));
+                break;
+            }
+        }
+        if !match_found {
+            result.push(input.remove(0));
+        }
+    }
+    result
 }
 
 pub struct GrammarRule {
