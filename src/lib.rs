5039e8f6938804ffce58ec1c256c784a3a465112
Gabriel Korherr <gabrielkorherr@gmail.com>
2024-06-22 18:29:12 +0200

    Basic shell struct

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,18 @@
 pub mod process_input;
+
+pub enum ResponseType {
+    Terminate,
+    Continue,
+}
+
+pub struct Response {
+    type: ResponseType
+}
+
+pub struct Shell;
+
+impl Shell {
+    pub fn process(&mut self, &[u8]) -> Response {
+        todo!()
+    }
+}
